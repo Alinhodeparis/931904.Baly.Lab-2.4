78ebe4a88a960bd2470888118a979e16c323028d
Baly Diabagate <diabagatebaly@gmail.com>
2022-04-16 16:12:41 +0700

    Ajoutez des fichiers projet.

---
+++ Lab_2_4 web tech/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Lab_2_4_web_tech.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
